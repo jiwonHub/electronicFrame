f8946e535c68a5bf4986a9dde64249bd45e76647
jiwonHub <jiwon31273@naver.com>
2022-12-26 22:47:49 +0900

    electronicFrame

--- app/src/main/java/com/example/electronicframe/MainActivity.kt
+++ app/src/main/java/com/example/electronicframe/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.electronicframe
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Contacts.Photo
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -30,6 +33,7 @@ class MainActivity : AppCompatActivity() {
             add(findViewById(R.id.imageView23))
         }
     }
+    private val imageUriList: MutableList<Uri> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,12 +54,23 @@ class MainActivity : AppCompatActivity() {
                     navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-                    showPermossionContextPopup()
+                    showPermissionContextPopup()
                 }
                 else ->{
                     requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
                 }
             }
+        }
+    }
+
+    private fun initStartPhotoFrameModeButton(){
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this, PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed{index, uri ->
+                intent.putExtra("photo$index", uri.toString())
+            }
+            intent.putExtra("photoListSize", imageUriList.size)
+            startActivity(intent)
         }
     }
 
@@ -85,8 +100,37 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, 2000)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            return
+        }
+
+        when(requestCode){
+            2000 ->{
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null){
+
+                    if(imageUriList.size == 6){
+                        Toast.makeText(this, "이미 사진이 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else ->{
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     @SuppressLint("NewApi")
-    private fun showPermossionContextPopup(){
+    private fun showPermissionContextPopup(){
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
             .setMessage("전자액자에 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
@@ -98,7 +142,5 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 
-    private fun initStartPhotoFrameModeButton(){
 
-    }
 }
